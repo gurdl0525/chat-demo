7b5dfb16801dddede1ac3250e3318cfa9f57bcf8
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 18:57:25 +0900

    ⭐️ :: TypeORMConfig

---
+++ src/app/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as process from 'process';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
