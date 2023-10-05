33d0498616ae310dfd859dd393cab3166ce57c4c
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-05 18:57:15 +0900

    📝 :: rename

--- src/app/domain/user/controller/dto/user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class SingUpRequest {
-  @IsString()
-  account_id: string;
-  @IsString()
-  password: string;
-}
---
+++ src/domain/auth/controller/dto/auth.dto.ts
@@ -0,0 +1,51 @@
+import { IsString, Matches } from 'class-validator';
+
+export class SingUpRequest {
+  @IsString({
+    message: 'string 이여야 합니다.',
+  })
+  @Matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+    message: (validationArguments) =>
+      `${validationArguments.property} : 올바른 이메일 형식이 아닙니다.`,
+  })
+  account_id: string;
+  @IsString({
+    message: 'string 이여야 합니다.',
+  })
+  @Matches(/(?=.*[a-zA-Z]{4,})(?=.*\d{4,})(?=.*[~!@#$%^*?]+).{7,16}/, {
+    message: (validationArguments) =>
+      `${validationArguments.property} : 비밀번호는 영문 4자이상, 숫자 4자 이상, 특수문자 1자 이상이 포함된 7자 이상 16자 이하의 문자열입니다.`,
+  })
+  password: string;
+}
+
+export class LogInRequest {
+  @IsString({
+    message: 'string 이여야 합니다.',
+  })
+  @Matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+    message: (validationArguments) =>
+      `${validationArguments.property} : 올바른 이메일 형식이 아닙니다.`,
+  })
+  account_id: string;
+  @IsString({
+    message: 'string 이여야 합니다.',
+  })
+  @Matches(/(?=.*[a-zA-Z]{4,})(?=.*\d{4,})(?=.*[~!@#$%^*?]+).{7,16}/, {
+    message: (validationArguments) =>
+      `${validationArguments.property} : 비밀번호는 영문 4자이상, 숫자 4자 이상, 특수문자 1자 이상이 포함된 7자 이상 16자 이하의 문자열입니다.`,
+  })
+  password: string;
+}
+
+export class TokenResponse {
+  access_token: string;
+  refresh_token: string;
+
+  constructor(accessToken?: string, refreshToken?: string) {
+    if (accessToken && refreshToken) {
+      this.access_token = accessToken;
+      this.refresh_token = refreshToken;
+    }
+  }
+}
