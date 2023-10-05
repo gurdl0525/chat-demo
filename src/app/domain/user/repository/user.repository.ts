b318e8b988bd3c72d86f7a26febeb774006558fa
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-05 18:57:48 +0900

    🔥 :: custom repo

--- src/app/domain/user/repository/user.repository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { User } from '../entity/user.entity';
-
-@EntityRepository(User)
-export class UserRepository extends Repository<User> {
-  async existByAccountId(accountId: string) {
-    return !!(await this.findOne({ where: { accountId } }));
-  }
-}
