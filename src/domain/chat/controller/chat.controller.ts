856c527550fc7d5514e1148172c1c57ec65eeb39
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-07 20:09:30 +0900

    ⭐️ :: sendChat, createChatRoom

--- src/domain/chat/controller/chat.controller.ts
+++ src/domain/chat/controller/chat.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Headers,
+  Res,
+  UseGuards,
+} from '@nestjs/common/decorators';
+import { Request, Response } from 'express';
+import { CreateChatRoomRequest, SendChatRequest } from './dto/chat.dto';
+import { ChatService } from '../service/chat.service';
+import { JwtAuthGuard } from '../../auth/jwt/jwt.guard';
+import { User } from '../../user/entity/user.entity';
 
-@Controller()
+@Controller('/chat')
 export class ChatController {
-  @Get()
-  root() {
-    return { message: 'Hello world!' };
+  constructor(private readonly chatService: ChatService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/room')
+  async createChatRoom(
+    @Headers('Authorization') token: string,
+    @Body() request: CreateChatRoomRequest,
+    @Res() res: Response,
+  ) {
+    return res
+      .status(201)
+      .json(await this.chatService.createRoom(request, token))
+      .send();
+  }
+
+  @Post()
+  async sendChat(
+    @Headers('Authorization') token: string,
+    @Body() request: SendChatRequest,
+    @Res() res: Response,
+  ) {
+    await this.chatService.sendChat(request, token);
+    return res.status(201).send();
   }
 }
