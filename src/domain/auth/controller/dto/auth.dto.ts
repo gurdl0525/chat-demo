55ca691f2171886027c83b94872d79dc32b7a895
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-07 20:08:21 +0900

    ✨ :: Validation

--- src/domain/auth/controller/dto/auth.dto.ts
+++ src/domain/auth/controller/dto/auth.dto.ts
@@ -1,17 +1,16 @@
-import { IsString, Matches } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class SingUpRequest {
-  @IsString({
-    message: 'string 이여야 합니다.',
-  })
+  @IsNotEmpty({ message: 'null일 수 없습니다.' })
+  @IsString({ message: 'string 이여야 합니다.' })
   @Matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
     message: (validationArguments) =>
       `${validationArguments.property} : 올바른 이메일 형식이 아닙니다.`,
   })
   account_id: string;
-  @IsString({
-    message: 'string 이여야 합니다.',
-  })
+
+  @IsNotEmpty({ message: 'null일 수 없습니다.' })
+  @IsString({ message: 'string 이여야 합니다.' })
   @Matches(/(?=.*[a-zA-Z]{4,})(?=.*\d{4,})(?=.*[~!@#$%^*?]+).{7,16}/, {
     message: (validationArguments) =>
       `${validationArguments.property} : 비밀번호는 영문 4자이상, 숫자 4자 이상, 특수문자 1자 이상이 포함된 7자 이상 16자 이하의 문자열입니다.`,
@@ -20,22 +19,25 @@ export class SingUpRequest {
 }
 
 export class LogInRequest {
-  @IsString({
-    message: 'string 이여야 합니다.',
-  })
+  @IsString({ message: 'string 이여야 합니다.' })
   @Matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
     message: (validationArguments) =>
       `${validationArguments.property} : 올바른 이메일 형식이 아닙니다.`,
   })
   account_id: string;
-  @IsString({
-    message: 'string 이여야 합니다.',
-  })
+
+  @IsString({ message: 'string 이여야 합니다.' })
   @Matches(/(?=.*[a-zA-Z]{4,})(?=.*\d{4,})(?=.*[~!@#$%^*?]+).{7,16}/, {
     message: (validationArguments) =>
       `${validationArguments.property} : 비밀번호는 영문 4자이상, 숫자 4자 이상, 특수문자 1자 이상이 포함된 7자 이상 16자 이하의 문자열입니다.`,
   })
   password: string;
+}
+
+export class ReIssueRequest {
+  @IsNotEmpty({ message: 'null일 수 없습니다.' })
+  @IsString({ message: 'string 이여야 합니다.' })
+  refresh_token: string;
 }
 
 export class TokenResponse {
