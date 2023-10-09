1ea6e0464f434b1570aefae7bcbf13821d9c0fee
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-09 23:30:50 +0900

    ⭐️ :: verifyUserByToken

---
+++ src/facade/user.facade.ts
@@ -0,0 +1,31 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../domain/user/entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
+import process from "process";
+
+export class UserFacade {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  verifyUser = async (token: string) => {
+    if (token.startsWith('Bearer ')) token = token.substring(7);
+
+    try {
+      const sub: string = (
+        await this.jwtService.verifyAsync(token, {
+          secret: process.env.SECRET_KEY,
+        })
+      ).sub;
+      return await this.userRepository.findOneByOrFail({
+        account_id: sub,
+      });
+    } catch (e) {
+      console.error(e);
+      throw new UnauthorizedException('올바르지 않은 토큰');
+    }
+  };
+}
