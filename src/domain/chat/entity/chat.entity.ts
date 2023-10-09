d60b0e477232cf0cab3b45570a911362c1c7eb53
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-09 23:33:15 +0900

    🐛 :: join에러 해결

--- src/domain/chat/entity/chat.entity.ts
+++ src/domain/chat/entity/chat.entity.ts
@@ -1,11 +1,4 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToMany,
-  PrimaryColumn,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { User } from '../../user/entity/user.entity';
 import { v4 as uuid } from 'uuid';
 
@@ -21,54 +14,49 @@ export class Room {
     type: 'binary',
     length: 36,
   })
-  id: string;
+  id!: string;
 
   @Column({ name: 'name', nullable: false })
-  name: string;
+  name!: string;
 
-  @OneToMany(() => Joiner, (joiner) => joiner.room_id)
-  joiner_list: Joiner[];
-
-  constructor(name?: string, joiner_list?: Joiner[], id?: string) {
+  constructor(name?: string, id?: string) {
     if (name) {
       this.id = id ?? uuid();
       this.name = name;
-      this.joiner_list = joiner_list ?? [];
     }
   }
 }
 
 @Entity({
   name: 'joiner',
-  orderBy: {
-    id: 'DESC',
-  },
 })
 export class Joiner {
-  @ManyToOne(() => Room, (room) => room.id)
-  @JoinColumn({ name: 'room_id' })
+  @ManyToOne(() => Room)
+  @JoinColumn({ name: 'room' })
+  room!: Room;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user' })
+  user!: User;
+
   @PrimaryColumn({
     type: 'binary',
     length: 36,
   })
   room_id!: string;
 
-  @ManyToOne(() => User, (user) => user.id)
-  @JoinColumn({ name: 'user_id' })
   @PrimaryColumn({
     type: 'binary',
     length: 36,
   })
   user_id!: string;
 
-  @OneToMany(() => Chat, (chat) => chat.joiner)
-  chat_list: Chat[];
-
-  constructor(room?: Room, user?: User, chat_list?: Chat[]) {
+  constructor(room?: Room, user?: User) {
     if (room && user) {
+      this.room = room;
+      this.user = user;
       this.room_id = room.id;
       this.user_id = user.id;
-      this.chat_list = chat_list ?? [];
     }
   }
 }
@@ -87,7 +75,7 @@ export class Chat {
   })
   id: string;
 
-  @ManyToOne(() => Joiner)
+  @ManyToOne(() => Joiner, { nullable: false })
   @JoinColumn([
     { name: 'room_id', referencedColumnName: 'room_id' },
     { name: 'user_id', referencedColumnName: 'user_id' },
