a369972fce5940cafa6a28485086a7810ff072eb
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-09 23:35:04 +0900

    🔥 :: enterRoom

--- src/domain/chat/service/chat.service.ts
+++ src/domain/chat/service/chat.service.ts
@@ -3,19 +3,18 @@ import {
   ConflictException,
   Injectable,
   NotFoundException,
-  UnauthorizedException,
 } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Repository } from 'typeorm/repository/Repository';
 import { Chat, Joiner, Room } from '../entity/chat.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../user/entity/user.entity';
-import { Socket } from 'socket.io';
 import {
   CreateChatRoomRequest,
   SendChatRequest,
 } from '../controller/dto/chat.dto';
 import * as fast from 'fast-deep-equal';
-import { JwtService } from '@nestjs/jwt';
+import { ChatGateway } from '../gateway/chat.gateway';
+import { UserFacade } from '../../../facade/user.facade';
 
 @Injectable()
 export class ChatService {
@@ -28,35 +27,12 @@ export class ChatService {
     private readonly userRepository: Repository<User>,
     @InjectRepository(Chat)
     private readonly chatRepository: Repository<Chat>,
-    private jwtService: JwtService,
+    private readonly chatGateway: ChatGateway,
+    private userFacade: UserFacade,
   ) {}
 
-  async enterRoom(roomId: string, sub: string, client: Socket) {
-    try {
-      const user = await this.userRepository.findOneBy({ account_id: sub });
-
-      if (!user) throw new UnauthorizedException();
-
-      const room = await this.roomRepository.findOneBy({ id: roomId });
-
-      if (!room) throw new BadRequestException();
-
-      const joiner = this.joinerRepository.findOneBy({
-        room_id: roomId,
-        user_id: user.id,
-      });
-
-      if (!joiner) throw new NotFoundException();
-    } catch (e) {
-      client.disconnect();
-      console.error('채팅방에 들어가 있지 않거나 존재하지 않는 채팅방입니다.');
-    }
-
-    client.rooms.add(roomId);
-  }
-
   async createRoom(req: CreateChatRoomRequest, token: string) {
-    const user = await this.jwtService.verifyAsync(token);
+    const user = await this.userFacade.verifyUser(token);
 
     const new_user = await this.userRepository.findOneBy({
       account_id: req.account_id,
@@ -79,20 +55,46 @@ export class ChatService {
   }
 
   async sendChat(request: SendChatRequest, token: string) {
-    const user = await this.jwtService.verifyAsync(token);
-    const room = await this.roomRepository.findOneByOrFail({
+    const user = await this.userFacade.verifyUser(token);
+
+    const room = await this.roomRepository.findOneBy({
       id: request.room_id,
     });
 
     if (!room) throw new NotFoundException('찾을 수 없는 채팅방입니다.');
 
-    const joiner = await this.joinerRepository.findOneBy({
+    const joiner: Joiner = await this.joinerRepository.findOneBy({
       room_id: room.id,
       user_id: user.id,
     });
 
     if (!joiner) throw new BadRequestException('채팅방에 참여하지 않았습니다.');
 
-    await this.chatRepository.save(new Chat(request.text, joiner));
+    const message = await this.chatRepository.save(
+      new Chat(request.text, joiner),
+    );
+
+    return this.chatGateway.server.to(room.id).emit('message', {
+      who: user.toResponse(),
+      message: message.text,
+      when: Date(),
+    });
   }
+
+  joinRoom = async (roomId: string, token: string) => {
+    const user = await this.userFacade.verifyUser(token);
+
+    const room = await this.roomRepository.findOneBy({ id: roomId });
+
+    if (!room) throw new NotFoundException('채팅방을 찾을 수 없습니다.');
+
+    if (
+      await this.joinerRepository.exist({
+        where: { room_id: room.id, user_id: user.id },
+      })
+    )
+      throw new ConflictException('이미 채팅방에 들어가 있습니다.');
+
+    await this.joinerRepository.save(new Joiner(room, user));
+  };
 }
